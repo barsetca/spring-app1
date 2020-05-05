d37ed939cfce5ee285238b68a683786840c5dd0c
Viktor Cherniak <barsetca@gmail.com>
2020-05-05 23:34:05 +0300

    lesson08 init method destroy method factory method

--- src/main/java/ru/cherniak/springlearn/ClassicalMusic.java
+++ src/main/java/ru/cherniak/springlearn/ClassicalMusic.java
@@ -1,8 +1,17 @@
 package ru.cherniak.springlearn;
 
 public class ClassicalMusic implements Music{
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
     @Override
     public String getSong() {
         return "Classical song";
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
     }
 }
