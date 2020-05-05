3544bba51f3ca0a92a4463be994c05030d5d180f
Viktor Cherniak <barsetca@gmail.com>
2020-05-06 00:15:18 +0300

    lesson09 <context:component-scan base-package="..." + @Component

--- src/main/java/ru/cherniak/springlearn/MusicPlayer.java
+++ src/main/java/ru/cherniak/springlearn/MusicPlayer.java
@@ -6,49 +6,49 @@ import java.util.List;
 public class MusicPlayer {
 
     //for DI from setter
-     private List<Music> musicList = new ArrayList<>();
-     private String name;
-     private int volume;
+     private Music music;
+//     private String name;
+//     private int volume;
 
-    public MusicPlayer() {
-    }
+//    public MusicPlayer() {
+//    }
 
     //IoC - inversion of control
-//    public MusicPlayer(Music music) {
-//        this.music = music;
-//    }
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
 //
 //    public void setMusic(Music music) {
 //        this.music = music;
 //    }
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getVolume() {
+//        return volume;
+//    }
+//
+//    public void setVolume(int volume) {
+//        this.volume = volume;
+//    }
+//
+//    public List<Music> getMusicList() {
+//        return musicList;
+//    }
+//
+//    public void setMusicList(List<Music> musicList) {
+//        this.musicList = musicList;
+//    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public List<Music> getMusicList() {
-        return musicList;
-    }
-
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
-    public void playMusic(){
-        for (Music music : musicList) {
+        public void playMusic() {
             System.out.println("Playing: " + music.getSong());
         }
     }
-}
+
+
